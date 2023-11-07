e65f6d315923437d315eb7376d505ea6eaf1145c
lematusiak <leematusiak@gmail.com>
2023-11-07 20:59:15 +0100

    Moduł 4, zadanie domowe

---
+++ MojeRozgrywki/MojeRozgrywki/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.ComponentModel.Design;
+using System.Threading.Tasks.Sources;
+
+namespace Lekcja1
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            var name = "Ewa";
+            var sex = "kobieta";
+            var age = 33;
+
+            if (sex == "kobieta" && age < 30)
+            {
+                Console.WriteLine("kobieta poniżej 30 lat");
+            }
+            else if (sex != "kobieta" && age < 18)
+            {
+                Console.WriteLine("niepełnoletni mężczyzna");
+            }
+            else if (sex == "kobieta" && age > 10 && age < 20)
+            {
+                Console.WriteLine("nastolatka");
+            }
+            else if (name == "Ewa" && age == 33)
+            {
+                Console.WriteLine("Ewa, lat 33");
+            }
+        
+ 
+        }
+
+    }
+
+
+
+
+
+
+
+}
