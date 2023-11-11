1778e840460ae98a2ded7fe0045734f103f1a8f7
lematusiak <leematusiak@gmail.com>
2023-11-11 15:42:26 +0100

    Kurs 021 Operatory Logiczne

--- MojeRozgrywki/MojeRozgrywki/Program.cs
+++ MojeRozgrywki/MojeRozgrywki/Program.cs
@@ -12,78 +12,43 @@ namespace Lekcja2
         {
 
             {
-                // PRZYPISYWANIE WARTOŚCI, ZMIENNA STAŁA, DZIAŁANIA +-:,
-                // INKREMENTACJA, DEKREMENTACJA, OPERATORY MATEMATYCZNE
+                // OPERATORY LOGICZNE
 
+                // Zmienna bool określa prawdę lub fałsz (true or false)
+                // bool a = 2 < 1; False  // bool a = 2 >1 True
 
-                // PIERWSZY SPOSÓB
+                // operator jednoargumentowy  - tzw negacja ponieważ określa jedno działanie, jedną liczbę
+                // liczbę, wartość - jak powyżej jedno true
+                // bool a = true; (daje true) bool a = !true (daje fals)
 
-                // string zmienna;       // deklaracja zmiennej tekstowej
-                // zmienna = "Lemalia";  // przypisanie wartości
+                // w c# znak = to przypisanie, == jest równy
+                //
+                // ZAIMPLEMENTOWANIE - oznacza, że kod programu został napisany
+                // i dostosoway do wykonania określonego zadania lub funkcji
+                //
+                // 1) KONIUNKCJA LOGICZNA &&  (i AND) czyli oba warunki muszą być spełnione
+                // bool a = 1 == 1 && 1 != 1; // wyskoczy false
+                // 2) ALTERNATYWA || (lub OR) tylko jeden spełniony wystarczy do prawdy true
+                // bool a = 1 == 1 || 1 != 1; // wyskoczy prawda
 
-                // DRUGI SPOSÓB
+                // KOLEJNOŚĆ WYKONYWANIA DZIAŁAŃ LOGICZNYCH
+                // Koniunkcja && wiąże MOCNIEJ niż Alternatywa
+                // bool a = true || true && false; // czyli tutaj prawda i fałsz jednocześnie daje fałsz - czyli alternatywa da true finalnie (bo prawda || fałsz - jeden warunek spełniony) 
+                // bool a = (true || true) && false; // dwie prawdy to również prawda ale z && false dają finalnie false
 
-                // można od razu zadeklarować zmienną i przypisać jej wartość
-                // do zmiennej stałej musimy przypisać od razu wartość
-                // zmienna const musi mieć określoną wartość - bo jest stałą
+                // DLA WARTOŚCI BEZWZGLĘDNYCH NIE MA PRZEWIDZIANYCH OSOBNYCH OPERATORÓW
+                // FUNKCJE METODY
 
+                Math.Pow(2,3); // Potęgownaie 2 do 3 = 8 finalnie daje 8
+                               // Console.WriteLine(Math.Pow(2,3));
+                Math.Sqrt(9);  // Pierwiastek finalnie daje 3
+                               // Console.WriteLine(Math.Sqrt(9));
+                Math.Abs(5);   // wartość bezwzględna zarówno z 5 i -5 daje 5
+                Console.WriteLine((5)); 
 
-                // const string stała = "Maraba";
-
-
-                // Console.WriteLine(zmienna); // wyskoczy - Lemalia
-                // Console.WriteLine(stała); // wyskoczy - Maraba
-                // Console.ReadKey();
-
-                int a = 2;
-                int b = 3;
-                int c1;
-                int c2;
-                int c3;
-                int c4;
-                int c5;
-                int c6;
-                int c7;
-
-                // a = a + 1; // powiększenie wartości a o 1 (razem 3)
-                // a += 1;    // powiększenie zapisane inaczej, wynik w wartości 1
-                // a++;       // INKREMENTACJA (zwiększ o 1), powiększenie zapisane inaczej
-                // a--;       // DEKREMENTACJA (zmniejsz o 1), wynik 1
-
-
-
-                // c1 = a + b;    // dodawanie 5
-                // a += b;        // dodawanie a+b ale wynik zostawiamy w zmiennej a zamiast c1
-
-                // c2 = a - b;    // odejmowanie -1
-                // a -= b;        // odejmowanie a-b, wynik w zmiennej a
-
-                // c3 = a * b;    // mnożenie 6
-                // a *= b;        // możenie a*b, wynik w zmiennej a
-
-                // c4 = b / a;   // dzielenie 1  (3:2, w trójce mamy jedną liczbę całkowitą dwójkę int, bez pozostałej reszty)
-                // b /= a;       // dzielenie b/a, wynik w zmiennej b
-
-                // c5 = a/b;     // dzielenie 0 (2:3, dwójka jest za mała, żeby było w niej choć jedno int 3, więc wynik zero)
-                // a /= b;       // dzielenie a/b, wynik w zmiennej a  
-
-                // c6 = b % a;    // reszta z dzielenia 3:2 pozostaje 1
-                // b %= a;        // reszta z dzielenia 3:2, wynik w zmiennej b
-
-                // c7 = a % b;    // reszta z dzielenia 2:3 2 jest za małe zmieścić 3, pozostaje 2)
-                // a %= b;        // reszta z dzielenia 2:3, wynik w zmiennej a
-
-
-                Console.WriteLine(a);
+            
                 Console.ReadKey();
-
-
-
-
-
-
-
-
+                // Podsumowując: do potęgowania, pierwiastkowania, itd nie mamy osobnych operatorów
 
             }
         
