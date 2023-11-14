c2e8803e43f8dab6e83de6348059af6027686468
lematusiak <leematusiak@gmail.com>
2023-11-14 16:35:40 +0100

    Kurs Lekcja 3 if, else, if, else INTO

--- MojeRozgrywki/MojeRozgrywki/Program.cs
+++ MojeRozgrywki/MojeRozgrywki/Program.cs
@@ -12,47 +12,108 @@ namespace Lekcja2
         {
 
             {
-                // OPERATORY LOGICZNE
+                // Zwracać typ danych oznacza, że funkcja, metoda lub wyrażenie zwraca wynik.
+                // To tak jak zamawiasz coś w restauracji, zwrócone jest danie.
 
-                // Zmienna bool określa prawdę lub fałsz (true or false)
-                // bool a = 2 < 1; False  // bool a = 2 >1 True
 
-                // operator jednoargumentowy  - tzw negacja ponieważ określa jedno działanie, jedną liczbę
-                // liczbę, wartość - jak powyżej jedno true
-                // bool a = true; (daje true) bool a = !true (daje fals)
+                // If bez żadnych klamr sprawdza tylko jedną instrukcję
+                // Sprawdza ją do najbliższego średnika;
+                // Pozostałe będą się wyświetlać, bez względu czy są prawdziwe
+                // Poniżej wyświetlą się 2x warunki prawdziwe, bo bool jest fałszywy
+                // int a = -1;
+                // bool b = a > 0;
+                // if (b) 
+                // Console.WriteLine("Warunek Prawdziwy");
+                // Console.WriteLine("Warunek Prawdziwy");
+                // Console.WriteLine("Warunek Prawdziwy");
 
-                // w c# znak = to przypisanie, == jest równy
+                // W klamrach zostaną sprawdzone wszystkie 3 warunki.
+                // Każdy warunek zostanie sprawdzony
+                // W przypadku prawdy wyświetli się 3 x Warunek Prawdziwy
+                // W przypadku fałszu nie wyświetli się nic
+                // int a = 1;
+                // bool b = a > 0;
+                // if (b)
+                // {
+                //   Console.WriteLine ("Warunek Prawdziwy");
+                //   Console.WriteLine ("Warunek Prawdziwy");
+                //   Console.WriteLine ("Warunek Prawdziwy");
+                // }
+
+                // int a = -1;
+                // bool b = a > 0;
+                // if (b)
+                // {
+                //    Console.WriteLine("Warunek Prawdziwy");
+                //    Console.WriteLine("Warunek Prawdziwy");
+                //    Console.WriteLine("Warunek Prawdziwy");
+                // }
+                // Dla przykładu można zastosować 2 ify
+
+                // int a = -1;
+
+                // if (a > 0) 
+                // {
+                //    Console.WriteLine("Mocne A" );
+                // }
+                // if (a < 0)
+                // {
+                //    Console.WriteLine("Słabitkie A");
+                // }
+
+                // Jeśli 1-szy if zostanie spełniony wyświetli się tylko pierwszy if, bez drugiego obojętnie na wynik
+                // Jeśli 1- szy if nie zostanie spełniony, rozpatrzony będzie 2-gi if
+
+                //int a = 1;
+
+                //if (a != 1)
+                //{
+                //    Console.WriteLine( "nieprawda");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("obojętnie czy prawda czy nie - wyświeli się");
                 //
-                // ZAIMPLEMENTOWANIE - oznacza, że kod programu został napisany
-                // i dostosoway do wykonania określonego zadania lub funkcji
-                //
-                // 1) KONIUNKCJA LOGICZNA &&  (i AND) czyli oba warunki muszą być spełnione
-                // bool a = 1 == 1 && 1 != 1; // wyskoczy false
-                // 2) ALTERNATYWA || (lub OR) tylko jeden spełniony wystarczy do prawdy true
-                // bool a = 1 == 1 || 1 != 1; // wyskoczy prawda
 
-                // KOLEJNOŚĆ WYKONYWANIA DZIAŁAŃ LOGICZNYCH
-                // Koniunkcja && wiąże MOCNIEJ niż Alternatywa
-                // bool a = true || true && false; // czyli tutaj prawda i fałsz jednocześnie daje fałsz - czyli alternatywa da true finalnie (bo prawda || fałsz - jeden warunek spełniony) 
-                // bool a = (true || true) && false; // dwie prawdy to również prawda ale z && false dają finalnie false
+                // Else zawsze się wykonuje, gdy if nie został spełniony
+                // Tłumaczy się go jako - w przeciwnym wypadku
 
-                // DLA WARTOŚCI BEZWZGLĘDNYCH NIE MA PRZEWIDZIANYCH OSOBNYCH OPERATORÓW
-                // FUNKCJE METODY
+                int a = 0;
+                if (a > 0)
+                {
+                    Console.WriteLine( "blisko");
+                }
+                else if (a == 0 && a !=1) // else if to inaczej dodatkowy if
+                {
+                    Console.WriteLine("w punkt");
+                    Console.WriteLine("i na pewno to nie jest jeden");
+                }
+                else
+                {
+                    Console.WriteLine("też bliko, ale jednak nie");
+                }
+                // W tym przypadku, będzie rozpatrzony if, póżniej else if 
+                // Ponieważ else if jest prawdziwy, program na tym pozostanie
+                // Gdyby nie drugi if również nie był prawdziwy, bez względu na wynik wyświetli się else 
+                // Krótko mówiąc: jeden spełniony if lub if else zamyka sprawę
+                // Przy dwóch warunkach spełnionych, gdy w dwóch klamrach zrobimy 
+                // console - wyświetlą się nam obie
 
-                Math.Pow(2,3); // Potęgownaie 2 do 3 = 8 finalnie daje 8
-                               // Console.WriteLine(Math.Pow(2,3));
-                Math.Sqrt(9);  // Pierwiastek finalnie daje 3
-                               // Console.WriteLine(Math.Sqrt(9));
-                Math.Abs(5);   // wartość bezwzględna zarówno z 5 i -5 daje 5
-                Console.WriteLine((5)); 
+                // if, else if i else zamyka całe działanie
+                // kolejny if po else otwiera nastepne 
 
-            
+                if (a != 2)
+                {
+                    Console.WriteLine( "też prawda");
+                }
+                else
+                {
+                    Console.WriteLine(  "co, nie?");
+                }
                 Console.ReadKey();
-                // Podsumowując: do potęgowania, pierwiastkowania, itd nie mamy osobnych operatorów
-
             }
-        
- 
+        // Wyświetli się - w punkt - i na pewno to nie jest jeden - też prawda
+        // Console.ReadKey(); ZAWSZE umieszczamy na KOŃCU wszelkich działań
         }
 
     }
