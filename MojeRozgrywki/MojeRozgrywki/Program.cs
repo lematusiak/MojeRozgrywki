659776de704c15bf115b00ac223d6302014da11b
lematusiak <leematusiak@gmail.com>
2023-11-21 12:39:44 +0100

    if, else if, else

--- MojeRozgrywki/MojeRozgrywki/Program.cs
+++ MojeRozgrywki/MojeRozgrywki/Program.cs
@@ -14,115 +14,35 @@ namespace Lekcja5
         {
 
             {
-                // Kurs 50, Lekcja 5, Konwersja typów, rzutowanie, komentarze
+                // Kurs 051, Lekcja 5, Konwersja typów, rzutowanie, komentarze
 
-                // KOMENTARZE
+                // if, else if, else
 
-                // Są ignorowane przez VS i język c#. Komentarze są dla nas.
-                // Pozwalają zarówno nam jak i zespołowi odczytać, co mieliśmy na myśli
-                // Nie są brane pod uwagę przy kompilacji i nie wpływają na działanie programu
+                bool isMan = true;
+                var age = 20;
+                string name = "Pawel";
 
-                // Jednolinikowy komentarz zaznaczany podwójnym sleszem
-
-                /* 
-                 * Wielolinijkowy komentarz - tutaj zaczynamy
-                 * piszemy, co chcemy
-                 * i tak ciągle i ciągle
-                 * aż kończymy
-                */
-
-                // SKRÓTY KLAWISZOWE & SNIPPETY
-
-                /*
-                 * Control S - zapisywanie obecnie otwartego dokumentu
-                 * Control C - kopiowanie zaznaczonego tekstu
-                 * Control V - wklejanie skopiowanego tekstu
-                 * 
-                 * Układanie kodu:    Control K  -  Control D
-                 * 
-                 * Zakomentowanie linijek kodu: Control K - Control C 
-                 * Odkomentowanie Control K - Control U
-                 * 
-                 * (w obu powyższych trzeba najpierw zaznaczyć tekst)
-                 * Odkomentowanie Control U (z angielskiego uncomment - odkomentuj)
-                 * 
-                 * f5 - rozpocznij, ale przy włączonej klawiaturze
-                 * (sprawdź JAK WŁĄCZYĆ klawisze funkcyjne f1,f2,f3 ...)
-                 * 
-                 * Aby włączyć klawisze F1, F2, F3 itd. w języku C#, 
-                 * możemy użyć funkcji o nazwie KeyPreview. 
-                 * Możemy to zrobić w różny sposób, ale prostym rozwiązaniem  
-                 * jest dodanie obsługi zdarzenia KeyDown dla formularza
-                 * lub kontrolki, na której chcemy obsłużyć te klawisze.
-                 * W c# można skorzystać z odpowiedniego programu dla każdego klawisza
-
-                 * SNIPPETY
-                 * 
-                 * Snippet (snipp - fragment) w programowaniu, zwłaszcza w języku C#, 
-                 * to krótki fragment kodu, który jest używany wielokrotnie w programie.
-                 * 
-                 * cw i 2x tab = Console.WriteLine();
-                 * 
-                 * for i 2x tab = for (int i = 0; i < 10; i++) (PĘTLA FOR)
-                 * 
-                 * while i 2x tab = (PĘTLA WHILE)
-                 * 
-                 * while (true)
-                 * {
-                 * 
-                 * }
-                 * PODOBNIE Z INNYMI - PRÓBUJ WSZYSTKIEGO z 2X TABULATOR
-                 * 
-                 * Ciekawostka: do i 2x tab daje: (PĘTLA DO WHILE)
-                 * 
-                do
+                if (isMan && age == 20)
                 {
-
-                } while (true);
-                 * 
-                 * Albo inna ciekawostka: (PĘTLA FOREACH)
-                 * 
-                foreach (var item in collection)  (var - element w kolekcji)
-                {
-
+                    Console.WriteLine("mężczyzna w wieku 20 lat");
                 }
-                 * 
-                 * UNIKANIE BUKSOWANIA W MULE, ZDERZENIA ZE ŚCIANĄ
-                 * 
-                 * Przy samej nauce, gdy masz jeszcze chłonny umysł unikaj wszelkich:
-                 * zafiksowań, buksowania, zakopania się w temacie, mitrężnenia czasu, 
-                 * na zagadnienia, których NIE MUSISZ TERAZ robić!
-                 * ODŁÓŻ rozwiązanie na potem, na wolną chwilę, na jałowy czas
-                 * np. w Tramwajach, lub ZAPYTAJ - jeśli można - kogoś, kto wytłumaczy
-                 * Przejrzyj turtoriale, livy, lekcje.
-                 * Reasumując: NIE MARNUJ CZASU, gdy nie trzeba. 
-                 * SZANUJ UMYSŁ, gdy jest chłonny do nauki! 
-                 * (bo nie zawsze jest chłonny, niekiedy też nie ma warunków do chłonności)
-                 * UCZ SIĘ (I ZAPISUJ) MAŁYMI PARTIAMI ... Wtedy to bardziej cieszy 
-                 * Wtedy dobrze masz w pamięci, co przerabiałeś
-                 
-                
-                 * czas 7.33
-
-
-                */
-
-
-
-                //kjsfjlh
-                //    fadfmnlkdsf
-                //    afdafds
-                //    afda
-
-
-                while (true)
+                else if (name == "Pawel" && age > 50)
                 {
+                    Console.WriteLine("Pawel po pięćdziesiątce");
+                }
+                else if ((isMan == false || isMan)   && age < 18)
+                {
+                    Console.WriteLine("niepełnoletnia osoba");
+                }
+                else
+                {
+                    Console.WriteLine("ktoś inny");
+                }
 
-                }  
 
-        
+                       
 
-               console.readkey();
+               Console.ReadKey();
             }
 
         }
